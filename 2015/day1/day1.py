beba6c81dd019a456be65133853459dc025d7951
Joel Petracci <jpetracci@gmail.com>
2019-12-29 01:00:00 -0800

    test updates

--- 2015/day1/day1.py
+++ 2015/day1/day1.py
@@ -1,5 +1,5 @@
 
-#global vars
+#globals
 floor = 0
 
 
@@ -25,8 +25,9 @@ def first_basement(moves):
         if floor < 0:
             return idx+1
 
-# input data
-with open('input.txt', mode='r') as f:
+
+# run with input data
+with open('2015/day1/input.txt', mode='r') as f:
     data = f.readline()
     # part 1
     process(data)
@@ -35,34 +36,40 @@ with open('input.txt', mode='r') as f:
     # part 2
     print(first_basement(data))
 
+
+'''
+tests
+'''
+
+
+def run_test_p1(test_data):
+    global floor
+    for route, end_floor in test_data:
+        process(route)
+        assert(floor == end_floor)
+
+
+def run_test_p2(test_data):
+    global floor
+    for route, basement_index in test_data:
+        assert(first_basement(route) == basement_index)
+
+
 # part 1 tests
-process('(())')
-assert(floor == 0)
+test_data_p1 = [['(())', 0],
+                ['()()', 0],
+                ['(((', 3],
+                ['(()(()(', 3],
+                ['))(((((', 3],
+                ['())', -1],
+                ['))(', -1],
+                [')))', -3],
+                [')())())', -3]]
 
-process('()()')
-assert(floor == 0)
-
-process('(((')
-assert(floor == 3)
-
-process('(()(()(')
-assert(floor == 3)
-
-process('))(((((')
-assert(floor == 3)
-
-process('())')
-assert(floor == -1)
-
-process('))(')
-assert(floor == -1)
-
-process(')))')
-assert(floor == -3)
-
-process(')())())')
-assert(floor == -3)
+run_test_p1(test_data_p1)
 
 # part 2 tests
-assert(first_basement(')') == 1)
-assert(first_basement('()())') == 5)
+test_data_p2 = [[')', 1],
+                ['()())', 5]]
+
+run_test_p2(test_data_p2)
