7a12580dd95dcf77e7cf86e0694726788b690f4d
Joel Petracci <jpetracci@gmail.com>
2020-12-13 21:18:12 -0800

    2020 day1 solution

---
+++ 2020/day1/src/main.rs
@@ -0,0 +1,43 @@
+use std::env;
+use std::fs;
+
+fn part1() {
+    let expense_report = fs::read_to_string("p1_input.txt").expect("error reading file");
+    let expense_report = expense_report.split("\n");
+    let values = expense_report.collect::<Vec<&str>>();
+
+    for i in 0..values.len() {
+        let val1 = values[i].parse::<i32>().unwrap_or(0);
+        for j in i..values.len() {
+            let val2 = values[j].parse::<i32>().unwrap_or(0);
+            let sum = val1 + val2;
+            if sum == 2020 {
+                println!("{} {} {}", val1, val2, val1 * val2);
+            }
+        }
+    }
+}
+
+fn part2() {
+    let expense_report = fs::read_to_string("p1_input.txt").expect("error reading file");
+    let expense_report = expense_report.split("\n");
+    let values = expense_report.collect::<Vec<&str>>();
+
+    for i in 0..values.len() {
+        let val1 = values[i].parse::<i32>().unwrap_or(0);
+        for j in i..values.len() {
+            let val2 = values[j].parse::<i32>().unwrap_or(0);
+            for k in j..values.len() {
+                let val3 = values[k].parse::<i32>().unwrap_or(0);
+                let sum = val1 + val2 + val3;
+                if sum == 2020 {
+                    println!("{}", val1 * val2 * val3);
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    part2();
+}
