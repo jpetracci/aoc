64fc5c58dac753a148dbb0aaee245f88cc4e4814
Joel Petracci <jpetracci@gmail.com>
2021-11-30 22:13:12 -0800

    day 1 part 2

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -14,10 +14,30 @@ fn part1(values: Vec<&str>) -> i32 {
     return sum;
 }
 
+fn part2(values: Vec<&str>) -> i32 {
+    let mut last = i32::MAX;
+    let mut sum = 0;
+
+    for i in 2..(values.len()) {
+        let v1 = values[i-2].parse::<i32>().unwrap_or(0);
+        let v2 = values[i-1].parse::<i32>().unwrap_or(0);
+        let v3 = values[i].parse::<i32>().unwrap_or(0);
+
+        let cur = v1 + v2 + v3;
+
+        if cur > last {
+            sum += 1;
+        }
+        last = cur;
+    }
+    return sum;
+}
+
 fn main() {
     let expense_report = fs::read_to_string("day1input.txt").expect("error reading file");
     let expense_report = expense_report.split("\n");
     let values = expense_report.collect::<Vec<&str>>();
 
-    println!("{}", part1(values));
+    println!("{}", part1(values.clone()));
+    println!("{}", part2(values));
 }
